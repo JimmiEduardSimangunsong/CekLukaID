87486ff6fa7cf83a743ed6eab28a8bfa1dbbca2b
Jimmi Eduard Simangunsong <jimmi24eduard@gmail.com>
2024-02-15 15:32:00 +0700

    ✅

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require ('sequelize');
 
-const sequelize = new Sequelize('ceklukaid_db', 'root', '',{
-    host: 'localhost',
+const sequelize = new Sequelize('ceklukaid_db', 'root', 'J1mm1Ceklukaid',{
+    host: '34.101.151.234',
     dialect: 'mysql'
 });
 
